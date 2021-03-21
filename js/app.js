8fc3ba3e36f2bcb91d735545921d6fad1a61d210
ISAsxm <huguesisabelle82@gmail.com>
2021-03-21 08:29:26 +0100

    optimize Js, add favicon

--- js/app.js
+++ js/app.js
@@ -1,15 +1,9 @@
 const navLinks = document.querySelectorAll(".navigation__link")
 const navToggle = document.querySelector("#nav-toggle")
+const listLength = navLinks.length
 
-const nodeListForEach = function (list, callback) {
-  listLength = list.length
-  for (let i = 0; i < listLength; i++) {
-    callback(list[i], i)
-  }
-}
-
-nodeListForEach(navLinks, function (current) {
-  current.addEventListener("click", function () {
+for (let i = 0; i < listLength; ++i) {
+  navLinks[i].addEventListener("click", function () {
     navToggle.checked = false
   })
-})
+}
